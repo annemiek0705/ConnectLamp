1ad90f5d4b070eb8a3ec9ecdd57ec2d5b12a33cb
Annemiek Fokke <aaamiek@gmail.com>
2023-02-25 23:00:06 +0100

    First version

--- LampServer.js
+++ LampServer.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
+let clientA, clientB;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/specialState', (req, res) => {
+	let response="";
+	if(clientA && !clientB) {
+		response = "Bfalse";
+	}else if(!clientA && clientB){
+		response = "Afalse";
+	}else if(clientA&&clientB){
+		response = "true"
+	}
+	res.send(response)
+})
+app.post('/clientstate', (req, res) => {
+	let clientstate = req.body;
+
+	if(clientstate.id == "A"){
+		if(clientstate.state){
+			clientA = true;
+		}else{
+			clientA = false;
+		}
+	}else if(clientstate.id =="B"){
+		if(clientstate.state){
+			clientB = true;
+		}else{
+			clientB = false;
+		}
+	}
+	res.send('Got a POST request')
 })
 
 app.listen(port, () => {
